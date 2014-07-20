b9fe38deb249117fe30ad5d6a449372c3ab05636
Matthew <mattjunk85@yahoo.com>
2014-07-20 12:54:06 -0700

    accounted for impossible triangles in js and updated the html formatting

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,19 @@
 var triFind = function(side1, side2, side3) {
-  if (side1%side2%side3 === 0) {return "Equilateral"}
-  else if (side1 === side2 || side2 === side3 || side1 === side3) {return "Isosceles"}
-  else {return "Scalene"}
+  if (side1 > side2 + side3 || side2 > side1 + side3 || side3 > side1 + side2) {
+    return "Impossible!";
+    console.log(true);
+  }
+  if (side1%side2 === 0 && side2%side3 === 0) {
+    return "Equilateral"
+  }
+  else if (side1 === side2 || side2 === side3 || side1 === side3) {
+    return "Isosceles"
+  }
+  else {
+    return "Scalene";
+  }
 };
+
+$(document).ready(function(){
+  $("form")
+})
