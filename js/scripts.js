3e4f5eddb4f1681d3688feb8ed409f8e1d185754
Matthew <mattjunk85@yahoo.com>
2014-07-20 13:55:09 -0700

    Created a visual triangle maker

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,34 @@
+// TRIANGLE CALCULATOR //
 var triFind = function(side1, side2, side3) {
-  if (side1 > side2 + side3 || side2 > side1 + side3 || side3 > side1 + side2) {
-    return "Impossible!";
-    console.log(true);
+  if (side1 > (side2 + side3) || side2 > (side1 + side3) || side3 > (side1 + side2)) {
+    return "an impossible triangle!";
   }
   if (side1%side2 === 0 && side2%side3 === 0) {
-    return "Equilateral"
+    return "an equilateral"
   }
   else if (side1 === side2 || side2 === side3 || side1 === side3) {
-    return "Isosceles"
+    return "an isosceles"
   }
   else {
-    return "Scalene";
+    return "a scalene";
   }
 };
 
+// BUTTON SUBMIT FUNCTION //
 $(document).ready(function(){
-  $("form")
-})
+  $("form#triangler").submit(function(event){
+    event.preventDefault();
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+    var result = triFind(side1,side2,side3);
+    $('.hideMe').show();
+    $('span#result').text(result + "!");
+    if (result === 'an impossible triangle!') {
+    } else {
+      $('#triangle').css({'border-left': (side1*25) + 'px solid transparent'});
+      $('#triangle').css({'border-right': (side2*25) + 'px solid transparent'});
+      $('#triangle').css({'border-bottom': (side3*25) + 'px solid red'});
+    }
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,19 +1,19 @@
 describe("triFind", function() {
   it("Tells you the type of triangle", function() {
-    triFind(5,5,5).should.equal("Equilateral");
-    triFind(2,2,2).should.equal("Equilateral");
+    triFind(5,5,5).should.equal("an equilateral");
+    triFind(2,2,2).should.equal("an equilateral");
   });
   it("Tells you the type of triangle", function() {
-    triFind(1,4,3).should.equal("Scalene");
-    triFind(2,6,7).should.equal("Scalene");
+    triFind(1,4,3).should.equal("a scalene");
+    triFind(3,4,5).should.equal("a scalene");
   });
   it("Tells you the type of triangle", function() {
-    triFind(7,2,7).should.equal("Isosceles");
-    triFind(3,3,4).should.equal("Isosceles");
+    triFind(7,2,7).should.equal("an isosceles");
+    triFind(3,3,4).should.equal("an isosceles");
   });
   it("Tells you when the dimensions are too long for a triangle", function() {
-    triFind(1,20,7).should.equal("Impossible!");
-    triFind(5,1,2).should.equal("Impossible!");
+    triFind(1,20,7).should.equal("an impossible triangle!");
+    triFind(5,1,2).should.equal("an impossible triangle!");
   });
 });
 
